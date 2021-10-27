6b4213a215e99dce665afe9a8d9bfeb39b986b24
amy890202 <52898990+amy890202@users.noreply.github.com>
2021-10-27 19:39:35 +0800

    Update and rename irisr_entropy_IG.py to decision_tree_entropy_IG.py

--- decision_tree_entropy_IG.py
+++ decision_tree_entropy_IG.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Oct 20 20:20:43 2021
+Created on Wed Oct 20 20:19:36 2021
 
-@author: user
+@author: USER
 """
 import math
-import datasets
+from sklearn import datasets
 
 def entropy(p1,n1):#算亂度
     if(p1==0 and n1==0):
@@ -16,26 +16,25 @@ def entropy(p1,n1):#算亂度
         pp = p1/(p1+n1)
         np = n1/(p1+n1)
         return -pp*math.log2(pp)-np*math.log2(np)
-    
-
 
 def IG(p1,n1,p2,n2):#計算information gain
-    num = p1 + n1 + p2 +n2
+    num = p1+n1+p2+n2
     num1 = p1+n1
     num2 = p2+n2
     return entropy(p1+p2,n1+n2)-num1/num*entropy(p1,n1)-num2/num*entropy(p2,n2)
 
-print(IG(3,3,2,2))#3+3- 2+2-
-print(IG(18,33,11,2))
-
-
-print(entropy(100,0))
-print(entropy(10,10))
-print(entropy(29,35))#2計算29+ 35- 的資料亂度
-
+# =============================================================================
+# print(IG(3,3,2,2))#3+3- 2+2-
+# print(IG(18,33,11,2))
+# 
+# 
+# print(entropy(100,0))
+# print(entropy(10,10))
+# print(entropy(29,35))#2計算29+ 35- 的資料亂度
+# =============================================================================
 data = datasets.load_iris()
-#list的第0個點是根節點 一開始所有資料都在0
-def ID3DT(feature, target):
+
+def ID3DT(feature,target):
     node = dict()
     node['data'] = range(len(target))
     tree = []
@@ -43,12 +42,12 @@ def ID3DT(feature, target):
     t = 0
     while(t<len(tree)):
         idx = tree[t]['data']
-        if(sum(target[idx]==0)):
+        if(sum(target[idx])==0):
             tree[t]['leaf']=1#如果IG是0時，或全部分類都一樣就是leaf(已知分類的節點)
             tree[t]['decision']=0
         elif(sum(target[idx])==len(idx)):
-            tree[t]['leaf']=1 
-            tree[t]['decision'] = 1
+            tree[t]['leaf']=1
+            tree[t]['decision']=1
         else:
             bestIG = 0
             for i in range(feature.shape[1]):#i跑每一個特徵
@@ -68,7 +67,7 @@ def ID3DT(feature, target):
                     p2 = sum(target[G2]==1)
                     n2 = sum(target[G2]==0)
                     thisIG = IG(p1,n1,p2,n2)
-                    if thisIG>bestIG:
+                    if(thisIG>bestIG):
                         bestIG = thisIG
                         bestG1 = G1
                         bestG2 = G2
@@ -76,20 +75,34 @@ def ID3DT(feature, target):
                         bestf = i
             if(bestIG>0):#如果最好的那刀大於0就要切
                 tree[t]['leaf']=0
-                tree[t]['selectf']= bestf
-                tree[t]['threshold']= bestthres
-                tree[t]['child']= [len(tree),len(tree)+1]
+                tree[t]['selectf']=bestf#用幾號特徵切
+                tree[t]['threshold']=bestthres
+                tree[t]['child']=[len(tree),len(tree)+1]
                 #tree新增兩個node, node放進資料
                 node = dict()
-                node['data'] = bestG1
+                node['data']=bestG1
                 tree.append(node)
                 node = dict()
-                node['data'] = bestG2
+                node['data']=bestG2
                 tree.append(node)
             else:#如果best的那刀還是0再切也沒用
-                tree[t]['leaf']=1
-                if sum(target[idx]==1) > sum(target[idx]==0):#1的個數比0多
-                    tree[t]['decision'] = 1
+                tree[t]['leaf']=1#已決定好分類
+                if(sum(target[idx]==1)>sum(target[idx]==0)):#1的個數比0多
+                    tree[t]['decision']=1
                 else:
-                    tree[t]['decision'] = 0
-        t += 1
+                    tree[t]['decision']=0
+        t+=1
+    return tree
+
+
+
+
+
+
+data = datasets.load_iris()
+feature = data['data']
+target = data['target']
+T= ID3DT(feature[50:150,:],target[50:150]-1)#(結果1跟2變0跟1)
+
+
+
